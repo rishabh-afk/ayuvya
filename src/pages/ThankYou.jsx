bc86399d27e03e308b0aac2d06ca2fd132ff0db9
Rishabh gupta <rishabh@innovatia.co.in>
2023-04-21 16:16:28 +0530

    make thank you page

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -1,13 +1,13 @@
-import CheckoutForm from "../components/common/forms/CheckoutForm";
 import OrderDetails from "../components/order/OrderDetails";
+import OrderSummary from "../components/order/OrderSummary";
 const ThankYou = () => {
   const handlePaymentType = () => {};
   return (
     <div className="lg:pl-20 flex flex-col lg:flex-row">
-      <div className="w-full md:w-[55%] p-8 lg:p-12 order-last lg:order-first">
-        <CheckoutForm handlePaymentType={handlePaymentType} />
+      <div className="w-full md:w-[55%] p-4 lg:p-12 order-last lg:order-first">
+        <OrderSummary />
       </div>
-      <div className="w-full md:w-[45%] px-8 lg:p-12 bg-gray-100 border-l border-[#e1e1e1] order-first lg:order-last">
+      <div className="w-full md:w-[45%] px-4 lg:p-12 bg-gray-100 border-l border-[#e1e1e1] order-first lg:order-last">
         <OrderDetails handlePaymentType={handlePaymentType} />
       </div>
     </div>
